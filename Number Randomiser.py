223beeb754884642d44d0a23f666ebfc13b0dda3
Hackcrack22 <104155490+Hackcrack22@users.noreply.github.com>
2023-12-11 14:17:17 +0100

    Add files via upload

---
+++ Number Randomiser.py
@@ -0,0 +1,8 @@
+# Créé par hacke, le 11/12/2023 en Python 3.7
+
+print("This tool is number randomiser. Please choose a number A and a number B")
+A=float(input("A = "))
+B=float(input("B = "))
+import random
+print("Here is your number : ",random.randint(A,B))
+
